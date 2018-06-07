67644a44329069282f0adff7818974f9df659af3
Andrew Stenyaew <andrej22116@hotmail.com>
2018-06-07 07:19:22 +0300

    Afff yeaa, added cool sound on background!

--- resource.h
+++ resource.h
@@ -3,6 +3,7 @@
 // Используется Coptyug_Points.rc
 //
 #define IDD_DIALOG1                     101
+#define IDR_WAVE1                       103
 #define RADIO_PVP                       1001
 #define RADIO_BOT_EASY                  1002
 #define RADIO_BOT_MEDIUM                1003
@@ -13,14 +14,13 @@
 #define BUTTON_WIDTH_ADD                1008
 #define BUTTON_WIDTH_SUB                1009
 #define EDIT_WIDTH_2                    1010
-#define IDC_CHECK1                      1011
 #define CHECK_SOUNDS                    1011
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1012
 #define _APS_NEXT_SYMED_VALUE           101
